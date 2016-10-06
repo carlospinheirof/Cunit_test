9c430caa2459fd2348a4dd5227756bfdb75d41db
carlospinheirof <carlos.pinheiro.f@gmail.com>
2016-10-06 00:41:07 -0300

    deixando amis bonito e separando

--- stack.c
+++ stack.c
@@ -1,37 +1,47 @@
-#include <stdio.h>
-#include <stdlib.h>
 
+#include "stack.h"
 struct _Stack 
 {
 	int index;
 	int st[250];
 };
-typedef struct _Stack Stack;
-
-//Cria uma pilha vazia
-Stack* create_stack();
-//coloca um item na pilha
-void push(Stack *stack, int item);
-//tira o item da pilha
-int pop(Stack *stack);
-//da uma olhada no primeiro item da pilha
-int peek(Stack *stack);
-// verifica se a pilha está vazia
-int is_empty(Stack *stack);
-
 int is_empty(Stack *stack)
 {
-	return(stack != NULL && stack->index >= 0);
+	return(stack == NULL || stack->index < 0);
 }
 
 Stack* create_stack()
 {
-	Stack *newStack = (*Stack) malloc(sizeof(Stack));
+	Stack *newStack = (Stack*) malloc(sizeof(Stack));
 	newStack->index = -1;
 	return newStack;
 }
 void push(Stack *stack, int item)
 {
+	if(stack->index == 249)
+	{
+		printf("It's full,sorry\n");
+		return;
+	}	
 	stack->st[++stack->index] = item;
 	return;
+}
+
+int peek(Stack *stack)
+{
+	if(!is_empty(stack)) return stack->st[stack->index];
+	else
+	{
+		printf("está vazia\n");
+		return -1;
+	}
+}
+int pop(Stack *stack)
+{
+	if(!is_empty(stack)) return stack->st[stack->index--];
+	else
+	{
+		printf("está vazia\n");
+		return -1;
+	}
 }
--- stack.h
+++ stack.h
@@ -1,12 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-/* Estrutura de dados pilha com array Autor Carlos Pinheiro */
 
-struct _Stack 
-{
-	int index;
-	int st[250];
-};
 typedef struct _Stack Stack;
 
 //Cria uma pilha vazia com no maximo 250 itens
@@ -19,44 +13,3 @@ int pop(Stack *stack);
 int peek(Stack *stack);
 // verifica se a pilha está vazia
 int is_empty(Stack *stack);
-
-int is_empty(Stack *stack)
-{
-	return(stack == NULL || stack->index < 0);
-}
-
-Stack* create_stack()
-{
-	Stack *newStack = (Stack*) malloc(sizeof(Stack));
-	newStack->index = -1;
-	return newStack;
-}
-void push(Stack *stack, int item)
-{
-	if(stack->index == 249)
-	{
-		printf("It's full,sorry\n");
-		return;
-	}	
-	stack->st[++stack->index] = item;
-	return;
-}
-
-int peek(Stack *stack)
-{
-	if(!is_empty(stack)) return stack->st[stack->index];
-	else
-	{
-		printf("está vazia\n");
-		return -1;
-	}
-}
-int pop(Stack *stack)
-{
-	if(!is_empty(stack)) return stack->st[stack->index--];
-	else
-	{
-		printf("está vazia\n");
-		return -987984;
-	}
-}
